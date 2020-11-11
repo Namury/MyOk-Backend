9cadf1d54acb7e570cd5834ad39416521fe3fd68
Namury <namuryah@gmail.com>
2020-11-11 09:11:27 +0700

    add migrations

---
+++ database/migrations/2020_11_11_013727_create_transaction_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->foreignId('transaction_id');
+            $table->foreignId('item_id');
+            $table->timestamps();
+
+            $table->foreign('transaction_id')->references('id')->on('transactions');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+}
